eb139a4349cb432d938f818f48ed2c491c4cdcc2
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2022-02-02 23:26:23 -0500

    Disable keyboard input while waiting for the jumps

--- src/jumper/__init__.py
+++ src/jumper/__init__.py
@@ -3,6 +3,7 @@ from random import randint
 import pyautogui
 import time
 import os
+import msvcrt
 
 
 def jump():
@@ -19,14 +20,35 @@ def clear_console():
     os.system(command)
 
 
+# Shamelessly ripped from
+# https://stackoverflow.com/questions/29289945/how-to-temporarily-disable-keyboard-input-using-python
+class KeyboardDisable:
+
+    def start(self):
+        self.on = True
+
+    def stop(self):
+        self.on = False
+
+    def __call__(self):
+        while self.on:
+            msvcrt.getwch()
+
+    def __init__(self):
+        self.on = False
+
+
 class Jumper:
     def __init__(self):
         self.iterations = 0
 
     def keep_jumping(self):
+        keyboardDisable = KeyboardDisable()
         while 1 == 1:
             clear_console()
             print("Close the tab to stop.")
             print(f"Amount of jumps currently at: {self.iterations}")
+            keyboardDisable.start()
             jump()
+            keyboardDisable.stop()
             self.iterations += 1
